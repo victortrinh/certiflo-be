a37433b433257c054107387b2d5c0e43a89dcae5
David Lépine <davidlep@users.noreply.github.com>
2020-01-26 10:52:33 -0500

    temp commit

---
+++ app/main/controller/job_posting_controller.py
@@ -0,0 +1,40 @@
+﻿from flask_restplus import Resource
+from flask import request
+from ..service.job_posting_service import get_job_postings, save_new_job_posting, update_job_posting, delete_job_posting
+from ..dto.job_postings_dto import JobPostingDTO
+
+api = JobPostingDTO.api
+
+
+@api.route('/getAll')
+class GetJobPostings(Resource):
+    @api.doc('Get job postings')
+    def get(self):
+        return get_job_postings()
+
+
+@api.route('/save')
+class SaveJobPosting(Resource):
+    @api.doc('Save new job posting')
+    @api.expect(JobPostingDTO.job_posting, validate=True)
+    def post(self):
+        data = request.json
+        return save_new_job_posting(data)
+
+
+@api.route('/update')
+class UpdateJobPosting(Resource):
+    @api.doc('Update the job posting')
+    @api.expect(JobPostingDTO.full_job_posting, validate=True)
+    def put(self):
+        data = request.json
+        return update_job_posting(data)
+
+
+@api.route('/delete')
+class DeleteJobPosting(Resource):
+    @api.doc('Delete job posting')
+    @api.expect(JobPostingDTO.job_posting_id, validate=True)
+    def delete(self):
+        data = request.json
+        return delete_job_posting(data)
