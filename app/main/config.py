e433923f2e4757a518702ebb2308a9ed8bb2c280
Victor Trinh <victor.trinh@gsoft.com>
2022-06-18 13:32:07 -0400

    Update database

--- app/main/config.py
+++ app/main/config.py
@@ -2,6 +2,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+stream = os.popen('heroku config:get DATABASE_URL -a certiflo-be')
+output = stream.read()
 
 class Config:
     SECRET_KEY = os.environ.get(
@@ -19,14 +21,14 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://lmqqsspkekuaar:d5de1f29813bdb355d79b824b5ad78c2af8be4c487317ca24885060824a0cfb2@ec2-54-208-158-56.compute-1.amazonaws.com:5432/d7448hsfkq2i2q'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or output
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://lmqqsspkekuaar:d5de1f29813bdb355d79b824b5ad78c2af8be4c487317ca24885060824a0cfb2@ec2-54-208-158-56.compute-1.amazonaws.com:5432/d7448hsfkq2i2q'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or output
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
