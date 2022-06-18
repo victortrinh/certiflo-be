c42abe669dcb291677a5ade2affd82b93d2b891f
Victor Trinh <victor.trinh@outlook.com>
2022-06-18 08:30:44 -0400

    Update database connection

--- app/main/config.py
+++ app/main/config.py
@@ -11,8 +11,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://tvyrdwzbnsjfdf:7ab783423f119fcdedd9534ec9f59a07afea1f02fe630c90f10' \
-        '06c88040432f1@ec2-107-20-168-237.compute-1.amazonaws.com:5432/d5c7g5fvfrekii'
+    SQLALCHEMY_DATABASE_URI = 'postgres://lmqqsspkekuaar:d5de1f29813bdb355d79b824b5ad78c2af8be4c487317ca24885060824a0cfb2@ec2-54-208-158-56.compute-1.amazonaws.com:5432/d7448hsfkq2i2q'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DISABLE_AUTHENTICATION = False
 
@@ -20,16 +19,14 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://tvyrdwzbnsjfdf:7ab783423f119fcdedd9534ec9f59a07afea1f02fe630c90f10' \
-                              '06c88040432f1@ec2-107-20-168-237.compute-1.amazonaws.com:5432/d5c7g5fvfrekii'
+    SQLALCHEMY_DATABASE_URI = 'postgres://lmqqsspkekuaar:d5de1f29813bdb355d79b824b5ad78c2af8be4c487317ca24885060824a0cfb2@ec2-54-208-158-56.compute-1.amazonaws.com:5432/d7448hsfkq2i2q'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://tvyrdwzbnsjfdf:7ab783423f119fcdedd9534ec9f59a07afea1f02fe630c90f10' \
-                              '06c88040432f1@ec2-107-20-168-237.compute-1.amazonaws.com:5432/d5c7g5fvfrekii'
+    SQLALCHEMY_DATABASE_URI = 'postgres://lmqqsspkekuaar:d5de1f29813bdb355d79b824b5ad78c2af8be4c487317ca24885060824a0cfb2@ec2-54-208-158-56.compute-1.amazonaws.com:5432/d7448hsfkq2i2q'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
