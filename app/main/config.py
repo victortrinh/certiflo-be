9b1b5b05768d4aa9b9208e44e1d9f51727e70077
victortrinh <31662661+victortrinh@users.noreply.github.com>
2023-01-02 14:53:04 -0500

    Update config.py

--- app/main/config.py
+++ app/main/config.py
@@ -13,7 +13,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://sfewlrtifivowy:3a328968685b4fe6d2a8ca7c146f4292eeaf21779d8c42bd9c032705d4cc49f0@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d2hai88lr5m2tl'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or output
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DISABLE_AUTHENTICATION = False
 
